ae6cfb74c1c6864640dcb469f332c2c79761cc89
Mark Tiahrt <mark.tiahrt@outlook.com>
2018-02-24 07:48:34 -0700

    missed the href link

--- src/App.js
+++ src/App.js
@@ -124,7 +124,7 @@ class App extends Component {
             }
 
           </div> : <button onClick={() => {
-            window.location = window.location.includes('localhost')
+            window.location = window.location.href.includes('localhost')
               ? 'http://localhost:8888/login' 
               : 'https://better-playlists-backend-mark.herokuapp.com/login'}
           }
